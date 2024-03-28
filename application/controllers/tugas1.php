16fd0da94a101bda04c59c0e0ac5f99ad5e1e834
Novi <yantinovi05604@gmail.com>
2024-03-28 12:20:26 +0700

    tugas 1

---
+++ application/controllers/tugas1.php
@@ -0,0 +1,12 @@
+<?php
+class tugas1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Nofi Yanti
+ Saya tinggal di daerah Pontianak
+ olah raga yang saya sukai adalah
+ Bulutangkis dan Makanan yang saya sukai adalah Bakso";
+ }
+}
