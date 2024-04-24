8007572c39bbc15b2271c8f3942418884a122f01
Novi <yantinovi05604@gmail.com>
2024-04-24 10:19:39 +0700

    pertemuan 5 tugas 4

--- application/views/view-form-matakuliah.php
+++ application/views/view-form-matakuliah.php
@@ -20,7 +20,7 @@
             <table>
                 <tr>
                     <th colspen="3">
-                        form Input Data Mata Kuliah
+                             form Input Data Mata Kuliah
                     </th>
                 </tr>
                 <td colspen="3">
@@ -28,30 +28,30 @@
                 </td>
                 </tr>
                 <tr>
-                    <th>KOde MTK</th>
+                    <th>Kode MTK</th>
                     <th>:</th>
                     <td>
-                        <input type="text" name="kode" id="kode">
-                    </td>
-                </tr>
-                <tr>
-                    <th>Nama MTK</th>
-                    <th>:</th>
-                    <td>
-                        <input type="text" name="kode" id="kode">
+                        <input type="text" name="kode" id="kode"
+                        class="<?= form_error('kode')  ? 'invalid'  : ' ' ?>"
+                            placeholder="isikan data" value="<?= set_value('kode')  ?>" />
+                        <div class="invalid-feedback"><?= form_error('kode') ?></div
                     </td>
                 </tr>
                 <tr>
                     <th>Nama MTK</th>
                     <td>:</td>
                     <td>
-                        <input type="text" name="nama" id="nama">
+                        <input type="text" name="nama" id="nama"
+                        class="<?= form_error('nama')  ? 'invalid'  : ' ' ?>"
+                            placeholder="isikan data" value="<?= set_value('nama')  ?>" />
+                        <div class="invalid-feedback"><?= form_error('nama') ?></div
                     </td>
                 </tr>
                 <tr>
                     <th>SKS</th>
                     <td>:</td>
                     <td>
+                    <div class="invalid-feedback"><?= form_error('sks') ?></div>
                         <select name="sks" id="sks">
                             <option value="">pilih SKS</option>
                             <option value="2">2</option>
